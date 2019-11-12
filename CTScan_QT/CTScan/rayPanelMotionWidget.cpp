faf49e7ab2f4b18626fdeae0ed94bcc5aa05d4b4
maozi2327 <maozi2327>
2019-11-12 10:23:40 +0800

    提交.gitignore

---
+++ CTScan_QT/CTScan/rayPanelMotionWidget.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "rayPanelMotionWidget.h"
+
+RayPanelMotion::RayPanelMotion()
+{
+	ui.setupUi(this);
+    
+    //setFixedHeight(500);
+    //ui.widget->setFixedHeight(500);
+    //QPoint point = ui.sampleChooseButton->pos();
+    //QRect rect = ui.sampleChooseButton->geometry();
+    //ui.widget->setFixedHeight(point.x() + rect.height());
+}
+
+
+void RayPanelMotion::on_sampleChooseButton_clicked()
+{
+}
+void RayPanelMotion::showEvent(QShowEvent* event)
+{
+	QPoint point = ui.sampleChooseButton->pos();
+	QRect rect = ui.sampleChooseButton->geometry();
+	setFixedHeight(point.y() + rect.height() + 20);
+}
